d1196a8b10450354d621874bdd2580a5dfb846d5
Eduardo <eduardorubio.ti@gmail.com>
2019-01-28 23:28:20 -0200

    First Commit

---
+++ src/main/java/br/com/rubioti/appspringmongo/AppSpringMongoApplication.java
@@ -0,0 +1,14 @@
+package br.com.rubioti.appspringmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringMongoApplication.class, args);
+	}
+
+}
+
